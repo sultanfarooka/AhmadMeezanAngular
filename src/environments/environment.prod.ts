d9e91c020c3a30805f0edcb8db99e67aeef3af97
iBrainSolution <info@ibrainsolution.com>
2022-05-25 12:48:18 +0200

    api endPoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://xyz_server:44382/api',
-  devEnv: 'local'
+  devEnv: 'remote',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   //apiUrl: 'https://wv1485:44392/api',
   apiUrl: 'http://localhost:3000/',
-  devEnv: 'local'   //local or remoteMachine
+  devEnv: 'remote', //local or remoteMachine
 };
 
 /*
